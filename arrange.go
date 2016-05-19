126a948fd46eead79c738080fce55fa6e0204916
Stephen McQuay (smcquay) <stephen@mcquay.me>
2016-05-18 21:04:21 -0700

    fixed bug if input dir doesn't exist.

--- arrange.go
+++ arrange.go
@@ -53,6 +53,9 @@ func Source(root string) <-chan string {
 		err := filepath.Walk(
 			root,
 			func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
 				if info.IsDir() {
 					return nil
 				}
